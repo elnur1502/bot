f2a34f73fb595f5e78f29ad5d47279ee075f06f9
elnur1502 <62846915+elnur1502@users.noreply.github.com>
2020-05-19 22:33:40 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,7 +14,7 @@ def get_page_data(html):
         td = tr.find_all('td')
         new = td[1].text.replace('\n\n\n\n\n\n','')
         sale = td[2].text.replace('\n','')
-        country = td[3].text.replace('\n\n\n\n\n\n','').replace('\r','').replace('**\n\n\n','')
+        country = td[3].text.replace('\n\n\n\n\n\n','').replace('\r','').replace('\n\n\n','')
         ru = td[4].text.replace('\n\n\n\n\n\nСША**\n\n','').replace('RUB\n\n','')
 
         data = {'new': new,
