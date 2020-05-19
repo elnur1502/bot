20ccdd30374842cf507c0eb9d6a44c5ee0b4fd91
elnur1502 <62846915+elnur1502@users.noreply.github.com>
2020-05-19 22:14:54 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,12 +1,5 @@
 import requests
 from bs4 import BeautifulSoup
-url = "https://www.xbox-now.com/ru/deal-list"
-ua = UserAgent()
-header = {
-    'User-Agent': ua.random,
-    'upgrade-insecure-requests': '1',
-    'cookie': 'mos_id=CllGxlx+PS20pAxcIuDnAgA=; session-cookie=158b36ec3ea4f5484054ad1fd21407333c874ef0fa4f0c8e34387efd5464a1e9500e2277b0367d71a273e5b46fa0869a; NSC_WBS-QUBG-jo-nptsv-WT-443=ffffffff0951e23245525d5f4f58455e445a4a423660; rheftjdd=rheftjddVal; _ym_uid=1552395093355938562; _ym_d=1552395093; _ym_isad=2'
-    }
 
 def get_html(site):
     r = requests.get(site)
