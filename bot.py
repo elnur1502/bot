e0a382ed54b63fd3a44f5b5455bba582c3fb350a
Elnur1502 <elnurshauketbek@gmail.com>
2020-05-19 19:43:38 +0500

    ok

---
+++ bot.py
@@ -0,0 +1,35 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+def get_html(site):
+    r = requests.get(site)
+    return r.text
+
+
+def get_page_data(html):
+    soup = BeautifulSoup(html, 'lxml')
+    line = soup.find('table', class='table table-striped').find('tbody').find_all('tr')
+
+    for tr in line:
+        td = tr.find_all('td')
+        new = td[1].text
+        sale = td[2].text
+        country = td[3].text.replace('\xa0', '')
+        ru = td[4].text.replace('\r\n        ', '')
+
+        data = {'new': ip,
+                'Скидка': port,
+                'Страна': country,
+                'в ру': ru}
+
+        print(data)
+
+
+def main():
+    url = 'https://www.xbox-now.com/ru/deal-list'
+    get_page_data(get_html(url))
+
+
+if __name__ == '__main__':
+    main()
