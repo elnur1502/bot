76c089a2106e7a70996ed09a6950c24fcb335597
elnur1502 <62846915+elnur1502@users.noreply.github.com>
2020-05-19 22:10:40 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
-
+url = "https://www.xbox-now.com/ru/deal-list"
+ua = UserAgent()
+header = {
+    'User-Agent': ua.random,
+    'upgrade-insecure-requests': '1',
+    'cookie': 'mos_id=CllGxlx+PS20pAxcIuDnAgA=; session-cookie=158b36ec3ea4f5484054ad1fd21407333c874ef0fa4f0c8e34387efd5464a1e9500e2277b0367d71a273e5b46fa0869a; NSC_WBS-QUBG-jo-nptsv-WT-443=ffffffff0951e23245525d5f4f58455e445a4a423660; rheftjdd=rheftjddVal; _ym_uid=1552395093355938562; _ym_d=1552395093; _ym_isad=2'
+    }
 
 def get_html(site):
     r = requests.get(site)
@@ -8,32 +14,23 @@ def get_html(site):
 
 
 def get_page_data(html):
-    try:  
-     soup = BeautifulSoup(html, 'lxml')
-     line = soup.find('table').find('tbody').find_all('tr')
+    soup = BeautifulSoup(html, 'lxml')
+    line = soup.find('table', class='table table-striped').find('tbody').find_all('tr')
 
-     for tr in line:
+    for tr in line:
         td = tr.find_all('td')
-        ip = td[1].text
-        port = td[2].text
-        country = td[3].text
-        anonym = td[4].text
-        types = td[5].text
-        time = td[6].text
+        new = td[1].text
+        sale = td[2].text
+        country = td[3].text.replace('\xa0', '')
+        ru = td[4].text.replace('\r\n        ', '')
 
-        data = {'ip': ip,
-                'Порт': port,
+        data = {'new': ip,
+                'Скидка': port,
                 'Страна': country,
-                'Анонимность': anonym,
-                'Тип': types,
-                'Время отклика': time}
-        
-        print(data)
-        
-    except AttributeError: return False
+                'в ру': ru}
 
-        
-    
+        print(data)
+
 
 def main():
     url = 'https://www.xbox-now.com/ru/deal-list'
