beffed143891586a34c170d9808ae0ebe7d5fe81
elnur1502 <62846915+elnur1502@users.noreply.github.com>
2020-05-19 23:02:12 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,15 +9,16 @@ def get_html(site):
 def get_page_data(html):
     soup = BeautifulSoup(html, 'lxml')
     line = soup.find('tbody').find_all('tr')
+    
 
     for tr in line:
         td = tr.find_all('td')
-        new = td[1].text.replace('\n\n\n\n\n\n','').replace('\nНОВИНКА\n\n\n','').replace('\n\n\n','').replace('\n\n','')
+        game = td.find_all('span')
         sale = td[2].text.replace('\n','')
         country = td[3].text.replace('\n\n\n\n\n\n','').replace('\r','').replace('\n\n\n','').replace('**\n\n                                    от:\n                                \n\n',' ').replace('RUB\n\n','')
         ru = td[4].text.replace('\n\n\n\n\n\nСША**\n\n','').replace('RUB\n\n','$').replace('\n\n\n\n\n\n','')
 
-        data = {'Игра': new,
+        data = {'Игра': game,
                 'Скидка': sale,
                 'Страна ': country,
                 'в ру': ru}
