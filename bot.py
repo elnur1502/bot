10814f1ab84c4f76f2e26972709a33deac0004e0
elnur1502 <62846915+elnur1502@users.noreply.github.com>
2020-05-20 21:53:51 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,10 @@
-
+import requests
 from bs4 import BeautifulSoup
+
+def get_html(site):
+    r = requests.get(site)
+    return r.text
+
 
 def get_page_data(html):
     soup = BeautifulSoup(html, 'lxml')
@@ -14,23 +19,20 @@ def get_page_data(html):
         sale = td[2].text.replace('\n','')
         bb = td[3].find('b')
         priceAU = bb.text.replace('\n','').replace('\n','').replace(' RUB','').replace(',','.').replace('\xa0','').replace('RUB','').replace('&nbsp;','')
-        priceA = float(float(priceAU)+30)
+        priceA = float(float(priceAU)*0.94*1.19)
         price = round(priceA)
         cc = td[4].find('b')
         priceRU = cc.text.replace('\n','').replace('\n','').replace(' RUB','').replace(',','.').replace('\xa0','').replace('RUB','').replace('&nbsp;','')
         priceR = float(float(priceRU)*72)
         priceU = round(priceR)
        
-        
-        
-        
-        
+      
         print(game + ' - ' + str(price) + ' рублей' + '.' + '(Скидка ' + sale+') ' + 'В магазине Майкрософт: ' + str(priceU) + ' рублей')
 
 
 def main():
-    url = open('ski.html', 'r')
-    get_page_data(url)
+    url = 'https://www.xbox-now.com/ru/deal-list'
+    get_page_data(get_html(url))
 
 
 if __name__ == '__main__':
