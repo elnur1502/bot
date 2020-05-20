12d26bcfaf665a8e9401b5650934ec96a4c55771
elnur1502 <62846915+elnur1502@users.noreply.github.com>
2020-05-20 19:26:06 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 f=open("skidki.html","r+")
 s=(f.read())
+soup = BeautifulSoup(html, 'lxml')
 line = s.find('tbody').find_all('tr')
 print(a)   #(если у вас в файле дание через пробел топишем  пробел, через точку-точку)
 f.close()
