0ed4209c83f65e64f6eca0d5f248f19cc7adea32
elnur1502 <62846915+elnur1502@users.noreply.github.com>
2020-05-20 19:24:02 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,39 +1,5 @@
-import requests
-from bs4 import BeautifulSoup
-
-def get_html(site):
-    r = requests.get(site)
-    return r.text
-
-
-def get_page_data(html):
-    soup = BeautifulSoup(html, 'lxml')
-    line = soup.find('tbody').find_all('tr')
-    
-
-    for tr in line:
-        td = tr.find_all('td')
-        td = tr.find_all('td')
-        aa = td[1].find('a')
-        game = aa.text.replace('\n','').replace('\n','').replace('НОВИНКА','')
-        sale = td[2].text.replace('\n','')
-        bb = td[3].find('b')
-        priceAU = bb.text.replace('\n','').replace('\n','').replace(' RUB','').replace(',','.').replace('\xa0','').replace('RUB','').replace('&nbsp;','')
-        priceA = float(float(priceAU)*0.94*1.19)
-        price = round(priceA)
-        cc = td[4].find('b')
-        priceRU = cc.text.replace('\n','').replace('\n','').replace(' RUB','').replace(',','.').replace('\xa0','').replace('RUB','').replace('&nbsp;','')
-        priceR = float(float(priceRU)*72)
-        priceU = round(priceR)
-       
-      
-        print(game + ' - ' + str(price) + ' рублей' + '.' + '(Скидка ' + sale+') ' + 'В магазине Майкрософт: ' + str(priceU) + ' рублей')
-
-
-def main():
-    url = open('skidki.html')
-    get_page_data(get_html(url))
-
-
-if __name__ == '__main__':
-    main()
+f=open("skidki.html","r+")
+s=(f.read())
+a=s.split(',')#вместо запятой может бить точка, любой символ
+print(a)   #(если у вас в файле дание через пробел топишем  пробел, через точку-точку)
+f.close()
