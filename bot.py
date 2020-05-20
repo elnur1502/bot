df72e6fe3dba6d28f8aea7d6c7429386a0ca2416
elnur1502 <62846915+elnur1502@users.noreply.github.com>
2020-05-20 12:05:29 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -17,7 +17,7 @@ def get_page_data(html):
         sale = td[2].text.replace('\n','')
         b = td[3].find('b')
         country = b.text.replace('\n','').replace('\n','')
-        priceRU = (float(float(country) * 100))
+        priceRU = (string(string(country) * 100))
         ru = td[4].text.replace('\n\n\n\n\n\nСША**\n\n','').replace('RUB\n\n','$').replace('\n\n\n\n\n\n','')
         
         
