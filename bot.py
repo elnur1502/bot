2631d128500682345a9fda204abac21cbc275625
elnur1502 <62846915+elnur1502@users.noreply.github.com>
2020-05-20 13:52:16 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -17,13 +17,13 @@ def get_page_data(html):
         game = aa.text.replace('\n','').replace('\n','')
         sale = td[2].text.replace('\n','')
         bb = td[3].find('b')
-        priceAU = len(bb.text.replace('\n','').replace('\n','').replace(' RUB',''))
+        priceAU = bb.text.replace('\n','').replace('\n','').replace(' RUB','')
        
         ru = td[4].text.replace('\n\n\n\n\n\nСША**\n\n','').replace('RUB\n\n','$').replace('\n\n\n\n\n\n','')
         data = {game + ' - ' + priceAU + ' рублей' + '.' + '(Скидка ' + sale+')'}
       
         print(data)
-        print(priceAU)
+        print(len(priceAU)*2)
 
 def main():
     url = 'https://www.xbox-now.com/ru/deal-list'
