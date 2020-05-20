00a5509011fddf8f1404b950dacc75a88b5cfeb2
elnur1502 <62846915+elnur1502@users.noreply.github.com>
2020-05-20 13:32:41 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,40 +1,4 @@
-import requests
-from bs4 import BeautifulSoup
-
-def get_html(site):
-    r = requests.get(site)
-    return r.text
-
-
-def get_page_data(html):
-    soup = BeautifulSoup(html, 'lxml')
-    line = soup.find('tbody').find_all('tr')
-    
-
-    for tr in line:
-        td = tr.find_all('td')
-        aa = td[1].find('a')
-        game = aa.text.replace('\n','').replace('\n','')
-        sale = td[2].text.replace('\n','')
-        bb = td[3].find('b')
-        priceAU = bb.text.replace('\n','').replace('\n','').replace(' RUB','')
-        type(int("priceAU"))
-        <class 'int'>
-        ru = td[4].text.replace('\n\n\n\n\n\nСША**\n\n','').replace('RUB\n\n','$').replace('\n\n\n\n\n\n','')
-        
-        
-        data = {game + ' - ' + priceAU + ' рублей' + '.' + '(Скидка ' + sale+')'}
-        
-      
-        
-        
-        print(data)
-
-
-def main():
-    url = 'https://www.xbox-now.com/ru/deal-list'
-    get_page_data(get_html(url))
-
-
-if __name__ == '__main__':
-    main()
+st_lst = ['121', '144', '111', '564']
+ 
+res_lst = [int(x) for x in st_lst]
+print (res_lst)
