9058469aeeae6174f544b03c06a11114bb2ff168
elnur1502 <62846915+elnur1502@users.noreply.github.com>
2020-05-20 20:04:34 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -16,8 +16,8 @@ def get_page_data(html):
         bb = td[3].find('b')
         try:
             priceAU = bb.text.replace('\n','').replace('\n','').replace(' RUB','').replace(',','.').replace('\xa0','').replace('RUB','').replace('&nbsp;','')
-                if str(bb.text.replace('\n','').replace('\n','').replace(' RUB','').replace(',','.').replace('\xa0','').replace('RUB','').replace('&nbsp;','')).find(",") != -1:
-                    priceAU = bb.text.replace('\n','').replace('\n','').replace(' RUB','').replace(',','.').replace('\xa0','').replace('RUB','').replace('&nbsp;','').split(",")[0]
+            if str(bb.text.replace('\n','').replace('\n','').replace(' RUB','').replace(',','.').replace('\xa0','').replace('RUB','').replace('&nbsp;','')).find(",") != -1:
+                priceAU = bb.text.replace('\n','').replace('\n','').replace(' RUB','').replace(',','.').replace('\xa0','').replace('RUB','').replace('&nbsp;','').split(",")[0]
         except:
             priceAU = 0
         priceA = float(float(priceAU)*0.94*1.19)
