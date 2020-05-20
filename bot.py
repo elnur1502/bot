52dd271112eb49e8481f49f291d44e94bff883a0
elnur1502 <62846915+elnur1502@users.noreply.github.com>
2020-05-20 12:04:45 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -16,13 +16,14 @@ def get_page_data(html):
         game = td[1].text.replace('\n\n\n\n\n\n','').replace('\nНОВИНКА\n\n\n','').replace('\n\n\n','').replace('\n\n','')
         sale = td[2].text.replace('\n','')
         b = td[3].find('b')
-        country = b.text
+        country = b.text.replace('\n','').replace('\n','')
+        priceRU = (float(float(country) * 100))
         ru = td[4].text.replace('\n\n\n\n\n\nСША**\n\n','').replace('RUB\n\n','$').replace('\n\n\n\n\n\n','')
         
         
         data = {'Игра': game,
                 'Скидка': sale,
-                'Страна ': country,
+                'Страна ': priceRU,
                 'в ру': ru}
 
         print(data)
