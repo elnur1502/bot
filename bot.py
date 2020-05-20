8c8d4daf769283c41312c5d9bc396de2ccd3886a
elnur1502 <62846915+elnur1502@users.noreply.github.com>
2020-05-20 19:04:09 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,10 +6,9 @@ def get_html(site):
     return r.text
 
 
-def html_string(html):
+def get_page_data(html):
     soup = BeautifulSoup(html, 'lxml')
     line = soup.find('tbody').find_all('tr')
-   
     
 
     for tr in line:
@@ -19,24 +18,21 @@ def html_string(html):
         game = aa.text.replace('\n','').replace('\n','').replace('НОВИНКА','')
         sale = td[2].text.replace('\n','')
         bb = td[3].find('b')
-        priceAU = bb.text.replace('\n','').replace('\n','').replace(' RUB','').replace(',','.').replace('\xa0','').replace('RUB','').replace('&nbsp;','').replace('&nbsp;','')
-        priceA = priceAU
-        price = round(float(float(priceA)*0.94*1.19))
+        priceAU = bb.text.replace('\n','').replace('\n','').replace(' RUB','').replace(',','.').replace('\xa0','').replace('RUB','').replace('&nbsp;','')
+        priceA = float(float(priceAU)*0.94*1.19)
+        price = round(priceA)
         cc = td[4].find('b')
         priceRU = cc.text.replace('\n','').replace('\n','').replace(' RUB','').replace(',','.').replace('\xa0','').replace('RUB','').replace('&nbsp;','')
         priceR = float(float(priceRU)*72)
         priceU = round(priceR)
        
-        if float(float(price)*2) < float(float(priceU)-200):
-            print(game + ' - ' + str(price + price) + ' рублей' + '.' + '(Скидка ' + sale+') ' + 'В магазине Майкрософт: ' + str(priceU) + ' рублей')
-        else:
-       
-            print(game + ' - ' + str(price) + ' рублей' + '.' + '(Скидка ' + sale+') ' + 'В магазине Майкрософт: ' + str(priceU) + ' рублей')
+      
+        print(game + ' - ' + str(price) + ' рублей' + '.' + '(Скидка ' + sale+') ' + 'В магазине Майкрософт: ' + str(priceU) + ' рублей')
 
 
 def main():
-   with open('skidki.html', 'r') as f: 
-        html_string = f.read()
+    url = 'https://www.xbox-now.com/ru/deal-list'
+    get_page_data(get_html(url))
 
 
 if __name__ == '__main__':
