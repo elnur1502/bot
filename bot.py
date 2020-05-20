8d6bbdfeef202029a7be0c0d39e3042c3e866684
elnur1502 <62846915+elnur1502@users.noreply.github.com>
2020-05-20 13:46:12 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -24,7 +24,7 @@ def get_page_data(html):
         data = {game + ' - ' + priceAU + ' рублей' + '.' + '(Скидка ' + sale+')'}
       
         print(data)
-        print(priceAU + 80)
+        print(int(priceAU) + 80)
 
 
 def main():
