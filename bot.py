92fe4496c66fe01e76dcf76c5ad601953482670e
elnur1502 <62846915+elnur1502@users.noreply.github.com>
2020-05-20 20:07:26 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,9 @@
-
+import requests
 from bs4 import BeautifulSoup
+
+def get_html(site):
+    r = requests.get(site)
+    return r.text
 
 
 def get_page_data(html):
@@ -14,22 +18,21 @@ def get_page_data(html):
         game = aa.text.replace('\n','').replace('\n','').replace('НОВИНКА','')
         sale = td[2].text.replace('\n','')
         bb = td[3].find('b')
-        priceAU = bb.text.replace('\n','').replace('\n','').replace(' RUB','').replace(',','.').replace('\xa0','').replace('RUB','').replace('&nbsp;','').split(",")[0]
-           
-       
+        priceAU = bb.text.replace('\n','').replace('\n','').replace(' RUB','').replace(',','.').replace('\xa0','').replace('RUB','').replace('&nbsp;','')
         priceA = float(float(priceAU)*0.94*1.19)
         price = round(priceA)
         cc = td[4].find('b')
-        priceRU = cc.text.replace('\n','').replace('\n','').replace(' RUB','').replace(',','.').replace('\xa0','').replace('RUB','').replace('&nbsp;','').split(",")[0]
+        priceRU = cc.text.replace('\n','').replace('\n','').replace(' RUB','').replace(',','.').replace('\xa0','').replace('RUB','').replace('&nbsp;','')
         priceR = float(float(priceRU)*72)
         priceU = round(priceR)
+       
       
         print(game + ' - ' + str(price) + ' рублей' + '.' + '(Скидка ' + sale+') ' + 'В магазине Майкрософт: ' + str(priceU) + ' рублей')
 
 
 def main():
-    url = open('skidki1.html', 'r')
-    get_page_data(url)
+    url = 'https://www.xbox-now.com/ru/deal-list'
+    get_page_data(get_html(url))
 
 
 if __name__ == '__main__':
