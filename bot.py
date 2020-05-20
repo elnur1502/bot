53dc3a3c86dbe6908994bbf1cb34780b882ffb83
elnur1502 <62846915+elnur1502@users.noreply.github.com>
2020-05-20 16:26:04 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,7 +15,7 @@ def get_page_data(html):
         td = tr.find_all('td')
         td = tr.find_all('td')
         aa = td[1].find('a')
-        game = aa.text.replace('\n','').replace('\n','')
+        game = aa.text.replace('\n','').replace('\n','').replace('НОВИНКА','')
         sale = td[2].text.replace('\n','')
         bb = td[3].find('b')
         priceAU = bb.text.replace('\n','').replace('\n','').replace(' RUB','').replace(',','.').replace('\xa0','').replace('RUB','')
