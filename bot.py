99ffdcc884a85f1599913f1feb48daaf374cef93
elnur1502 <62846915+elnur1502@users.noreply.github.com>
2020-05-20 12:15:30 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -18,7 +18,7 @@ def get_page_data(html):
         b = td[3].find('b')
         AU = [e.text for e in b.children if e.name is not None]
         country = str(AU.replace('\n','').replace('\n',''))
-        priceRU = float(float(country) + 200))
+        priceRU = country.text
         ru = td[4].text.replace('\n\n\n\n\n\nСША**\n\n','').replace('RUB\n\n','$').replace('\n\n\n\n\n\n','')
         
         
