dfca8edee533317ec98da8a63bc156c0ef547c07
elnur1502 <62846915+elnur1502@users.noreply.github.com>
2020-05-20 15:00:45 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,7 +15,7 @@ def get_page_data(html):
         td = tr.find_all('td')
       
         bb = td[3].find('b')
-        priceAU = bb.text.replace('\n','').replace('\n','').replace(' RUB','').replace(',','.').replace('\xa0','')
+        priceAU = bb.text.replace('\n','').replace('\n','').replace(' RUB','').replace(',','.').replace('\xa0','').replace('RUB','')
         price = round(float(priceAU))
         
         
