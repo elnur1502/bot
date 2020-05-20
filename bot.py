f6b19ef474e9e333d8c1b72f495975aa2dce6228
elnur1502 <62846915+elnur1502@users.noreply.github.com>
2020-05-20 19:13:57 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,39 @@
 import requests
 from bs4 import BeautifulSoup
-f = open("skidki.html", "r")
-x = 0
-for line in f:
-    if x == 1:
-        print(line.split()[3])
-    x += 1
 
-f.close()
+def get_html(site):
+    r = requests.get(site)
+    return r.text
+
+
+def get_page_data(html):
+    soup = BeautifulSoup(html, 'lxml')
+    line = soup.find('tbody').find_all('tr')
+    
+
+    for tr in line:
+        td = tr.find_all('td')
+        td = tr.find_all('td')
+        aa = td[1].find('a')
+        game = aa.text.replace('\n','').replace('\n','').replace('НОВИНКА','')
+        sale = td[2].text.replace('\n','')
+        bb = td[3].find('b')
+        priceAU = bb.text.replace('\n','').replace('\n','').replace(' RUB','').replace(',','.').replace('\xa0','').replace('RUB','').replace('&nbsp;','')
+        priceA = float(float(priceAU)*0.94*1.19)
+        price = round(priceA)
+        cc = td[4].find('b')
+        priceRU = cc.text.replace('\n','').replace('\n','').replace(' RUB','').replace(',','.').replace('\xa0','').replace('RUB','').replace('&nbsp;','')
+        priceR = float(float(priceRU)*72)
+        priceU = round(priceR)
+       
+      
+        print(game + ' - ' + str(price) + ' рублей' + '.' + '(Скидка ' + sale+') ' + 'В магазине Майкрософт: ' + str(priceU) + ' рублей')
+
+
+def main():
+    f = open("skidki.html", "r")
+    get_page_data(get_html(f))
+
+
+if __name__ == '__main__':
+    main()
