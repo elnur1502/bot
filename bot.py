3b1d0b6eabdbc08553be2a3f6dce1eb8b9a15507
elnur1502 <62846915+elnur1502@users.noreply.github.com>
2020-05-20 14:03:22 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -19,7 +19,7 @@ def get_page_data(html):
         bb = td[3].find('b')
         priceAU = bb
         
-        data = {game + ' - ' + priceAU + ' рублей' + '.' + '(Скидка ' + sale+')'}
+        data = {game + ' - ' + str(priceAU) + ' рублей' + '.' + '(Скидка ' + sale+')'}
       
         print(data)
         
