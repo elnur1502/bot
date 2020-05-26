46dcf7ef86196565b22830620fdf270aaf5c1a0f
elnur1502 <62846915+elnur1502@users.noreply.github.com>
2020-05-26 21:29:07 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,17 +1,20 @@
-# -*- coding: utf-8 -*-
+import requests
 from bs4 import BeautifulSoup
+
+def get_html(site):
+    r = requests.get(site)
+    return r.text
+
 
 def get_page_data(html):
     soup = BeautifulSoup(html, 'lxml')
     line = soup.find('tbody').find_all('tr')
-    
-
     for tr in line:
         td = tr.find_all('td')
         td = tr.find_all('td')
         aa = td[1].find('a')
-        game = aa.text.replace('\n','').replace('\n','')
-        sale = td[2].text.replace('\n','').replace(' (GOLD)','')
+        game = aa.text.replace('\n','').replace('\n','').replace('НОВИНКА','')
+        sale = td[2].text.replace('\n','')
         bb = td[3].find('b')
         priceAU = bb.text.replace('\n','').replace('\n','').replace(' RUB','').replace(',','.').replace('\xa0','').replace('RUB','').replace('&nbsp;','')
         priceA = float(float(priceAU)+30)
@@ -20,9 +23,8 @@ def get_page_data(html):
         priceRU = cc.text.replace('\n','').replace('\n','').replace(' RUB','').replace(',','.').replace('\xa0','').replace('RUB','').replace('&nbsp;','')
         priceR = float(float(priceRU)*72)
         priceU = round(priceR)
-       
-        if float(price) < float(priceU):
-            if float(float(price)*2) < float(float(priceU)-200):
+       	
+	if float(price) < float(priceU):if float(float(price)*2) < float(float(priceU)-200):
                 print(game + ' - ' + str(round(float(price)*2)) + ' рублей' + '.' + '(Скидка ' + sale+') ' + 'В магазине Майкрософт: ' + str(priceU) + ' рублей')
             else:
                 if float(float(price)*1.95) < float(float(priceU)-200):
@@ -159,14 +161,17 @@ def get_page_data(html):
                                                                                                                                                                                                         else:
                                                                                                                                                                                                             print('') 
 
-                                                                                                                                                    
-                                                                                                                                                    
-                                                                                                                                                                        
+        
+        
+        
         
 
+    
+
+
 def main():
-    url = open('ski.html', 'r')
-    get_page_data(url)
+    url = 'https://www.xbox-now.com/ru/deal-list'
+    get_page_data(get_html(url))
 
 
 if __name__ == '__main__':
