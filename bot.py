f4ac2dced98036e2a4ac49c8e80efa05ace0235d
elnur1502 <62846915+elnur1502@users.noreply.github.com>
2020-06-05 11:32:20 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,31 +3,17 @@ from bs4 import BeautifulSoup
 
 def get_page_data(html):
     soup = BeautifulSoup(html, 'lxml')
-    line = soup.find('tbody').find_all('tr')
+    line = soup.find('div', 'row')
+    divs5 = line.find_all('div', 'col-xs-12 col-lg-5')
+    divs6 = line.find_all('div', 'col-xs-12 col-lg-6')
+    for div in divs5:
+    	aa = div.find('a').find('span').text.replace("вЂ","'").replace("в„ў","").replace("В®","").replace("™","").replace("Ђ","'").replace("пј†","&").replace("Bundle","Комплект").replace(" '“ "," ").replace("Game of The Year Edition","Издание «Игра года»")
+        print(aa)
+    print(divs6)
     
-
-    for tr in line:
-        td = tr.find_all('td')
-        aa = td[1].find('a')
-        game = aa.text.replace('\n','').replace('\n','')
-        sale = td[2].text.replace('\n','').replace(' (GOLD)','')
-        bb = td[3].find('b')
-        priceAU = bb.text.replace('\n','').replace('\n','').replace(' RUB','').replace(',','').replace('\xa0','').replace('RUB','').replace('&nbsp;','')
-        priceA = float(float(priceAU)+30)
-        price = round(priceA)
-        cc = td[4].find('b')
-        priceRU = cc.text.replace('\n','').replace('\n','').replace(' RUB','').replace(',','.').replace('\xa0','').replace('RUB','').replace('&nbsp;','')
-        priceR = float(float(priceRU)*72)
-        priceU = round(priceR)
-       
-        if float(price) < float(priceU):
-            if float(float(price)*2) < float(float(priceU)-200):
-                print(game + ' - ' + str(round(float(price)*2)) + ' rub' + '.' + '(Skidka ' + sale+') ' + 'ssd: ' + str(priceU) + ' rub')
-            else:
-                print('')                                                                                                               
-                                                                                                                                                                        
-        
-
+    
+    
+    
 def main():
     url = open('ski.html', 'r')
     get_page_data(url)
